33f1fb8d79f3e4683830893c8003afe4e53ef9aa
ArtyomRusak <bembi1204@gmail.com>
2013-12-21 16:04:48 +0300

    [Update]: fix. [ArtyomRusak]

--- Lab5.EPAM/Lab5.EPAM.EFData/UnitOfWork.cs
+++ Lab5.EPAM/Lab5.EPAM.EFData/UnitOfWork.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Data.Entity;
 using Lab5.EPAM.Core;
+using Lab5.EPAM.Core.Entities;
 using Lab5.EPAM.Core.Exceptions;
+using Lab5.EPAM.Core.InterfaceRepository;
+using Lab5.EPAM.EFData.Repositories;
 
 namespace Lab5.EPAM.EFData
 {
@@ -13,6 +16,8 @@ namespace Lab5.EPAM.EFData
         private bool _isTransactionActive;
         private readonly DbContext _context;
         private readonly DbContextTransaction _transaction;
+        private IRepository<User, int> _userRepository;
+        private IRepository<Role, int> _roleRepository;
 
         #endregion
 
@@ -33,6 +38,23 @@ namespace Lab5.EPAM.EFData
 
         public void Dispose()
         {
+            if (_isTransactionActive)
+            {
+                try
+                {
+                    _context.SaveChanges();
+                    _transaction.Commit();
+                    _isTransactionActive = false;
+                }
+                catch (Exception e)
+                {
+                    _transaction.Rollback();
+                    _context.Dispose();
+                    _disposed = true;
+                    _isTransactionActive = false;
+                    throw new RepositoryException(e);
+                }
+            }
             if (!_disposed)
             {
                 _context.Dispose();
@@ -94,6 +116,20 @@ namespace Lab5.EPAM.EFData
         public void PreSave()
         {
             _context.SaveChanges();
+        }
+
+        #endregion
+
+        #region Implementation of IRepositoryFactory
+
+        public IRepository<User, int> GetUserRepository()
+        {
+            return _userRepository ?? (_userRepository = new Repository<User, int>(_context));
+        }
+
+        public IRepository<Role, int> GetRoleRepository()
+        {
+            return _roleRepository ?? (_roleRepository = new Repository<Role, int>(_context));
         }
 
         #endregion
